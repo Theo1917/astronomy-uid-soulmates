41052d9368a7517439f198959758d9459a696dc5
Theo1917 <manojsrivatsav2006@gmail.com>
2024-05-20 16:16:16 +0530

    first commit

---
+++ e l/Login Page/login.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const wrapper = document.querySelector('.wrapper');
+    const loginLink = document.querySelector('.login-link');
+    const registerLink = document.querySelector('.register-link');
+    registerLink.addEventListener('click', () => {
+        wrapper.classList.add('active');
+    });
+
+    loginLink.addEventListener('click', () => {
+        wrapper.classList.remove('active');
+    });
+});
+    btnPopup.addEventListener('click', () => {
+    wrapper.classList.add('active-popup');
+});
